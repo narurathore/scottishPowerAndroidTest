fff677eeaf487eb2937b867945f11ec5abba0f58
Narayan Singh <narurathore@gmail.com>
2025-03-17 22:33:17 +0000

    dispatcher di module added

---
+++ app/src/main/java/com/narayan/singh/scottishpowerandroidtest/common/di/DispatcherModule.kt
@@ -0,0 +1,24 @@
+package com.narayan.singh.scottishpowerandroidtest.common.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DispatcherModule {
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
