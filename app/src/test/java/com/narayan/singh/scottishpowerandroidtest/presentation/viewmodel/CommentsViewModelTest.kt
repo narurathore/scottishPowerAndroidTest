ab952776bcf7d2420bb03bb80fedac6644cec1a0
Narayan Singh <narurathore@gmail.com>
2025-03-17 22:49:10 +0000

    comments viewmodel test cases improved

--- app/src/test/java/com/narayan/singh/scottishpowerandroidtest/presentation/viewmodel/CommentsViewModelTest.kt
+++ app/src/test/java/com/narayan/singh/scottishpowerandroidtest/presentation/viewmodel/CommentsViewModelTest.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.narayan.singh.scottishpowerandroidtest.presentation.viewmodel
 
 import com.narayan.singh.scottishpowerandroidtest.common.MainDispatcherRule
@@ -5,7 +7,10 @@ import com.narayan.singh.scottishpowerandroidtest.domain.model.Comment
 import com.narayan.singh.scottishpowerandroidtest.domain.usecase.GetCommentsUseCase
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.*
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -27,15 +32,27 @@ class CommentsViewModelTest {
             Comment(1, 1, "User1", "user1@example.com", "Test comment"),
             Comment(2, 1, "User2", "user2@example.com", "Another test")
         )
-        coEvery { getCommentsUseCase() } returns commentsList
+        coEvery { getCommentsUseCase() } coAnswers {
+            delay(100) // so all states of ui state can be captured
+            commentsList
+        }
         viewModel = CommentsViewModel(getCommentsUseCase, testDispatcherRule.testDispatcher)
     }
 
     @Test
-    fun `viewModel should fetch comments on initialization`() = runTest {
-        // Then: UI state should have updated with comments
-        val state = viewModel.uiState.first()
-        assertEquals(false, state.isLoading)
-        assertEquals(2, state.comments.size)
+    fun `isLoading should be true initially and then false when comments are fetched`() = runTest {
+        val stateChanges = mutableListOf<CommentsUIState>()
+        val testScope = TestScope()
+        val job = testScope.launch(testDispatcherRule.testDispatcher) {
+            viewModel = CommentsViewModel(getCommentsUseCase, testDispatcherRule.testDispatcher)
+            viewModel.uiState.toList(stateChanges)
+        }
+
+        advanceUntilIdle()
+
+        job.cancel()
+
+        assert(stateChanges.any { it.isLoading })
+        assertEquals(false, stateChanges.last().isLoading)
     }
 }
