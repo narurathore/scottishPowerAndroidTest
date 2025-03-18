365160f8b4da047446c7ca498f360c4e14e7563b
Narayan Singh <narurathore@gmail.com>
2025-03-18 11:01:35 +0000

    certification error resolved. it should be resolved by backend. but to continue working on resolved it here

--- app/src/main/java/com/narayan/singh/scottishpowerandroidtest/di/AppModule.kt
+++ app/src/main/java/com/narayan/singh/scottishpowerandroidtest/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.narayan.singh.scottishpowerandroidtest.di
 
+import android.annotation.SuppressLint
 import com.narayan.singh.scottishpowerandroidtest.data.network.CommentApi
 import com.narayan.singh.scottishpowerandroidtest.data.repository.CommentRepositoryImpl
 import com.narayan.singh.scottishpowerandroidtest.data.utils.ApiConstants
@@ -9,19 +10,51 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import javax.inject.Singleton
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
+    fun provideUnsafeOkHttpClient(): OkHttpClient {
+        val trustAllCerts = arrayOf<TrustManager>(
+            @SuppressLint("CustomX509TrustManager")
+            object : X509TrustManager {
+                @SuppressLint("TrustAllX509TrustManager")
+                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                @SuppressLint("TrustAllX509TrustManager")
+                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+            }
+        )
+
+        val sslContext = SSLContext.getInstance("TLS").apply {
+            init(null, trustAllCerts, SecureRandom())
+        }
+
+        val sslSocketFactory = sslContext.socketFactory
+
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
+    }
+
+    @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(ApiConstants.BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
